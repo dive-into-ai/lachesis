16a649ab03cb8a573c880de9dbe45015bd0ebb82
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-05-18 23:36:27 +0900

    Add 메트릭 추가가
    - val_pearson
    - f1_score
    
    #2

--- model/metric.py
+++ model/metric.py
@@ -9,7 +9,6 @@ def accuracy(output, target):
         correct += torch.sum(pred == target).item()
     return correct / len(target)
 
-
 def top_k_acc(output, target, k=3):
     with torch.no_grad():
         pred = torch.topk(output, k, dim=1)[1]
@@ -17,4 +16,26 @@ def top_k_acc(output, target, k=3):
         correct = 0
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
+    return correct / len(target)
+
+'''
+val_pearson 추가
+'''
+def val_pearson(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
+    return correct / len(target)
+
+'''
+f1_score 추가
+'''
+def f1_score(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
     return correct / len(target)
