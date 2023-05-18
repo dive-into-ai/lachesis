4ef9c99a158f0148e8e5cec06b4679e5ec4ed634
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-05-18 23:35:20 +0900

    Add 모델 추가
    - CifarModel 모델 추가
    - TextSimilarityModel 모델 추가
    - TextGenerationModel 모델 추가
    
    #2

--- model/model.py
+++ model/model.py
@@ -1,8 +1,15 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from base import BaseModel
 
 
+'''
+이미지 분류 모델
+'''
 class MnistModel(BaseModel):
     def __init__(self, num_classes=10):
         super().__init__()
@@ -20,3 +27,84 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+
+'''
+Cifar10 데이터셋을 위한 모델
+'''
+class CifarModel(BaseModel):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(64 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 64 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+'''
+텍스트 유사도 분석 모델
+'''
+class TextSimilarityModel(BaseModel):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_classes):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        output, _ = self.lstm(embedded)
+        output = output[:, -1, :]
+        logits = self.fc(output)
+        return F.softmax(logits, dim=1)
+
+
+'''
+텍스트 생성 모델
+'''
+class TextGenerationModel(BaseModel):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, x, hidden):
+        embedded = self.embedding(x)
+        output, hidden = self.lstm(embedded, hidden)
+        output = output[:, -1, :]
+        logits = self.fc(output)
+        return logits, hidden
+
+    def generate_text(self, start_sequence, length, temperature=1.0):
+        self.eval()
+
+        with torch.no_grad():
+            inputs = torch.tensor([[start_sequence]])
+            hidden = None
+            generated_text = [start_sequence]
+
+            for _ in range(length):
+                logits, hidden = self.forward(inputs, hidden)
+                logits = logits[:, -1, :] / temperature
+                probs = F.softmax(logits, dim=1)
+                predicted = torch.multinomial(probs, num_samples=1)
+                generated_text.append(predicted.item())
+                inputs = predicted
+
+        self.train()
+
+        return generated_text
